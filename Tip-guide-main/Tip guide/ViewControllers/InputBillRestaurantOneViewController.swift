c92ea6e04e71070392c14529b18410bb1d5bd5b4
Rashmi Rao <67630400+rashmi16267@users.noreply.github.com>
2021-06-06 21:37:03 +0530

    Add files via upload

---
+++ Tip-guide-main/Tip guide/ViewControllers/InputBillRestaurantOneViewController.swift
@@ -0,0 +1,39 @@
+//
+//  InputBillRestaurantOneViewController.swift
+//  Tip guide
+//
+//  Created by Rashmi Rao on 31/05/21.
+//
+
+import Foundation
+
+import UIKit
+
+public class InputBillRestaurantOneViewController : UIViewController {
+    @IBOutlet weak var billAmount : UITextField!
+    var bill : String = ""
+    var billAmt : Int = 0
+    
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Input Bill"
+        bill = billAmount.text!
+        billAmt = Int(bill)!
+        billAmount.keyboardType = .numberPad
+    }
+    
+    @IBAction func buttonClick(sender: UIButton) {
+        self.performSegue(withIdentifier: "submit", sender: self)
+        
+    }
+    
+    
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MySegueID" {
+            if let destination = segue.destination as? RateExperienceRestaurantOneViewController {
+                destination.billAmt = self.billAmt
+            }
+        }
+    }
+}
